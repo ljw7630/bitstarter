c9604a33d978f7e5c2cf0d9d7792fab4e5ae02f2
Jinwu <ljw7630@hotmail.com>
2013-07-19 01:20:24 +0100

    HW4

--- grader.js
+++ grader.js
@@ -24,8 +24,10 @@
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler')
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://quiet-taiga-8319.herokuapp.com/"
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,8 +67,20 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_link>', 'Url to page', null, URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    var f = program.file
+    if(f == null) {
+        rest.get(program.url).on('complete', function(result) {
+           if(result instanceof Error) {
+               this.retry(500);
+           } else {
+              f = result;
+           }
+        });
+    }
+
+    var checkJson = checkHtmlFile(f, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
